d91caeddb59632ef5459c2ed489c582ab586003a
Goran <gsarajlic@gmail.com>
2018-08-25 21:52:41 +0200

    start again

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
-componentDidMount() {
-  this.initMap()
-}
-
-// Initialize and add the map which has been centered on Zagreb, Croatia
-initMap = () => {
-  // The location of Zagreb,Croatia
-  const zagreb = { lat: 45.815011, lng: 15.981919  };
-  // The map, centered at Zagreb
-  const map = new window.google.maps.Map(
-    document.getElementById('map'), 
-      { zoom: 12, center: zagreb }
-    );
-  // The marker, positioned at Zagreb
- var marker = new window.google.maps.Marker({ position: zagreb, map: map });
-}
+  loadMap = () => {
+    mapLoadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDBAVP-AGB5WzPHx4SdmJFDs5z1Thd3EQ4&callback=initMap')
+  }
 
 
+  initMap = () => {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    })
+  }
 
 
   render() {
@@ -29,8 +22,17 @@ initMap = () => {
       <main>
         <div id="map"></div>
       </main>
-    );
+    )
   }
+}
+
+function mapLoadScript (url) {
+  let paramOne = window.document.getElementsByTagName ('script'),
+      script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  paramOne.parentNode.insertBefore(script, paramOne)
 }
 
 export default App;
