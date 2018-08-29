c47cf2469cbb61429fee0c431e971ba822f1a258
Goran <gsarajlic@gmail.com>
2018-08-29 19:23:02 +0200

    corrected list items for aria

--- src/components/SightList.js
+++ src/components/SightList.js
@@ -11,7 +11,11 @@ class SightList extends Component {
         return (
             <ul id="list" type="text" name="List of locations">
                 {this.props.sights.map(s =>
-                    <button className="listbutton" aria-label="press for sight info" key={s.venue.id} onClick={this.handleClick} value={s.venue.name} name="">{s.venue.name}</button>
+                <li tabIndex={0} 
+                    className="listbutton"
+                    key={s.venue.id} onClick={this.handleClick} 
+                    value={s.venue.name}> {s.venue.name}
+                </li>
                     ) 
                 }
             </ul>
